0d03a07a567335e8f0485e91efc2a1ba0f444520
stoianurania <stoianurania@yaoo.com>
2018-11-12 21:56:38 +0200

    show pages based on selected menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+function showHomePage() {
+  document.getElementById("skills-page").style.display="none";
+  document.getElementById("languages-page").style.display="none";
+  document.getElementById("home-page").style.display="block";
+}
+function showSkillsPage() {
+  document.getElementById("home-page").style.display="none";
+  document.getElementById("languages-page").style.display="none";
+  document.getElementById("skills-page").style.display="block";
+} 
+function showLanguagesPage() {
+  document.getElementById("home-page").style.display="none";
+  document.getElementById("skills-page").style.display="none";
+  document.getElementById("languages-page").style.display="block";
+}
+
+
+
+document.getElementById("home-menu").onclick = showHomePage
+document.getElementById("skills-menu").onclick = showSkillsPage;
+document.getElementById("languages-menu").onclick = showLanguagesPage;
